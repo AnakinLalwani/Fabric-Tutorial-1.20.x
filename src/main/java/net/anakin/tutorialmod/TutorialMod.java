b8175383ae27151c96650febbf296b7e788ac8e9
AnakinLalwani <72091898+AnakinLalwani@users.noreply.github.com>
2025-04-08 14:38:11 +0530

    Ballin

--- src/main/java/net/anakin/tutorialmod/TutorialMod.java
+++ src/main/java/net/anakin/tutorialmod/TutorialMod.java
@@ -14,7 +14,7 @@ public class TutorialMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		LOGGER.info("Ballin");
 		LOGGER.info("Hello Fabric world!");
 	}
 }
